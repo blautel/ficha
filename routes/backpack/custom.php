7cfd3a8d32453c0b207d25fefe516bba97f4a022
Xavi Baz <xavi@aula2030.es>
2020-08-08 13:58:29 +0200

    CRUD inicial jornadas

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,5 @@ Route::group([
     ],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('jornada', 'JornadaCrudController');
 }); // this should be the absolute last line of this file
