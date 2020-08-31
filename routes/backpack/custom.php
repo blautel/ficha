ade6584ab60c996802cc968d8e7030b7630f6324
blautel <info@javiles.dev>
2020-08-31 19:11:50 +0200

    #23 añadido un nuevo grupo de rutas para separar los accesos

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,6 +6,22 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+//Grupo para acceso "admin"
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => [
+        config('backpack.base.web_middleware', 'web'),
+        config('backpack.base.middleware_key', 'admin'),
+        'can:gest-accesos',
+    ],
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('jornada', 'JornadaCrudController');
+    Route::crud('proyecto', 'ProyectoCrudController');
+    Route::crud('tarea', 'TareaCrudController');
+});
+
+//Grupo para acceso normal
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => [
@@ -13,8 +29,7 @@ Route::group([
         config('backpack.base.middleware_key', 'admin'),
     ],
     'namespace'  => 'App\Http\Controllers\Admin',
-], function () { // custom admin routes
+], function () {
     Route::crud('jornada', 'JornadaCrudController');
-    Route::crud('proyecto', 'ProyectoCrudController');
-    Route::crud('tarea', 'TareaCrudController');
+
 }); // this should be the absolute last line of this file
