f00c16ccf138a090179a8e08779707e7c64b6358
blautel <info@javiles.dev>
2020-08-31 10:09:22 +0200

    #16 Crud inial de proyectos y tareas
    Se ha realizado el crud inicial de proyectos y tareas, también se han cambiado los iconos de dichos crud.

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,7 +1,9 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('jornada') }}'><i class='nav-icon la la-question'></i> Jornadas</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('jornada') }}'><i class='nav-icon la la-hourglass-start'></i> Jornadas</a></li>
 @can('gest-accesos')
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('proyecto') }}'><i class='nav-icon la la-project-diagram'></i> Proyectos</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('tarea') }}'><i class='nav-icon la la-tasks'></i> Tareas</a></li>
 <li class="nav-item nav-dropdown">
 	<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Acceso</a>
 	<ul class="nav-dropdown-items">
@@ -11,3 +13,5 @@
 	</ul>
 </li>
 @endcan
+
+
